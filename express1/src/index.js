e5289e89a063fbf95221b632627590b07adaf466
Alex Gabriel Malisa <malisaag370@gmail.com>
2024-03-22 02:36:23 +0300

    get and post usage in express

--- express1/src/index.js
+++ express1/src/index.js
@@ -3,3 +3,17 @@ const PORT=3001;
 const app = express();
 
 app.listen(PORT,()=>console.log(`server is up running listening port : ${PORT}`));
+app.use(express.json());
+const usersList=[{"name":"Alex Malisa"},{"name":"frank Malisa"}]
+app.get('/users',(req,res)=>{
+    console.log(req);
+    res.send(usersList);
+});
+
+app.post('/users',(req,res)=>{
+    console.log(req.body);
+    usersList.push(req.body);
+    res.sendStatus(201);
+});
+
+
